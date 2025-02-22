6c29e8fb83b366417a0cccca6d9fdcd7895af1ad
Jacob Dearing <jacob.dearing@gmail.com>
2025-02-22 21:32:43 +0000

    rename load-config to config-file and enable log-level

--- main.go
+++ main.go
@@ -14,11 +14,30 @@ var argVersion = flag.Bool("version", false, "print debug info and exit")
 
 var heartBeat = flag.Duration("heartbeat", 1*time.Second, "duration between checks")
 var initConfig = flag.Bool("init-config", false, "initialize and save a new config file")
-var loadConfig = flag.String("load-config", "go-live-reload.json", "load a config file")
+var configFile = flag.String("config-file", "go-live-reload.json", "load a config file")
+var logLevel = flag.String("log-level", "info", "log level (debug, info, warn, error)")
 
 func main() {
 
 	flag.Parse()
+
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}
+
+	switch *logLevel {
+	case "debug":
+		opts.Level = slog.LevelDebug
+	case "info":
+		opts.Level = slog.LevelInfo
+	case "warn":
+		opts.Level = slog.LevelWarn
+	case "error":
+		opts.Level = slog.LevelError
+	default:
+		slog.Error("main", "log-level", *logLevel)
+		return
+	}
 
 	if *argVersion {
 		version()
@@ -27,23 +46,23 @@ func main() {
 
 	if *initConfig {
 		c := NewConfig()
-		err := c.Save(*loadConfig)
+		err := c.Save(*configFile)
 		if err != nil {
 			slog.Error("main/init-config", "error", err)
 			return
 		}
-		slog.Info("main/init-config", "config", *loadConfig)
+		slog.Info("main/init-config", "config", *configFile)
 		return
 	}
 
 	config := &Config{}
-	err := config.Load(*loadConfig)
+	err := config.Load(*configFile)
 	if err != nil {
-		slog.Error("main/load-config", "error", err)
+		slog.Error("main/config-file", "error", err)
 		return
 	}
 
-	slog.Info("main ready", "load-config", *loadConfig)
+	slog.Info("ready", "config-file", *configFile)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
