604bcb7b5a63a7078933dff97bf3f24320dce482
Jacob Dearing <jacob.dearing@gmail.com>
2025-02-26 06:35:52 -0600

    go specific hack to build mywebserver[.exe] and call it in an agnostic way

--- config.go
+++ config.go
@@ -28,13 +28,38 @@ func NewConfig() *Config {
 				Match:       []string{"*.go"},
 				HeartBeat:   time.Duration(1 * time.Second),
 				BuildCmd:    "go",
-				BuildArgs:   []string{"build", "-o", "build/webserver"},
-				BuildEnv:    []string{"CGO_ENABLED=0"},
-				BuildDir:    ".",
-				RunCmd:      "./webserver",
-				RunArgs:     []string{"--www-bind", ":8081", "--www-root", "wwwroot"},
-				RunEnv:      []string{"WWWBIND=8081", "WWWROOT=wwwroot"},
-				RunDir:      "build",
+
+				/*
+					go help build: If the named output is an existing directory or
+					ends with a slash or backslash, then any resulting executables
+					will be written to that directory.  The '.exe' suffix is added
+					when writing a Windows executable.
+				*/
+				BuildArgs: []string{"build", "-o", "build/"},
+				BuildEnv:  []string{"CGO_ENABLED=0"},
+				BuildDir:  ".",
+
+				/*
+					Windows would look for webserver, find webserver.exe, check
+					if PATH match it by [*.com, *.exe] etc, and run it.
+					Linux would look for webserver, find it *if* its walked as
+					./webserver, and run it. Otherwise, it would look in PATH.
+
+					win: search for ./webserver, find ./webserver.exe, run it.
+					win: search for webserver, executable file not found in %PATH%.
+					linux: search for ./webserver, find ./webserver, run it.
+					linux: search for webserver, executable file not found in $PATH.
+
+					So with go build we cheat by having Go name the executable,
+					using the build -o build/ with a trailing slash. This way, the
+					executable is named webserver or webserver.exe, and we can target
+					it directly with run command as ./webserver.
+				*/
+
+				RunCmd:  "./webserver",
+				RunArgs: []string{"--www-bind", ":8081", "--www-root", "wwwroot"},
+				RunEnv:  []string{"WWWBIND=8081", "WWWROOT=wwwroot"},
+				RunDir:  "build",
 			},
 		},
 	}
